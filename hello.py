e67020b4795f918f9862b37ba10ca4e6755b03ac
Kelvin <whkelvin@github.com>
2017-11-19 21:43:08 -0800

    removed irrevelent file

--- hello.py
+++ hello.py
@@ -6,6 +6,19 @@ class car:
         self.row = row
         self.col = col
         self.ready_to_win = False
+        self.update_domain_and_range()
+    def contain_point(self, pos):
+        if pos[0] >= self.domain[0] and pos[0] <=  self.domain[1] and pos[1] >= self.range[0] and pos[1] <= self.range[1]:
+            return True
+        else:
+            return False
+    def update_domain_and_range(self):
+        if self.orientation == "v":
+            self.domain = [80*self.col, 80* self.col + 80]
+            self.range = [80*self.row, 80*self.row + 80*self.size]
+        elif self.orientation == "h":
+            self.domain = [80*self.col, 80* self.col + 80 * self.size]
+            self.range = [80*self.row, 80* self.row + 80]
 
 class game:
     num_of_car = 1
@@ -35,12 +48,7 @@ class game:
         self.car_list.append(car)
 
     def move_car(self, id, direction):
-        if id == 0:
-            #this is the winning car
-            if self.car_list[id].row == 1 and self.car_list[id].col == 5:
-                self.car_list[id].ready_to_win = True
-            else:
-                self.car_list[id].ready_to_win = False
+
         if id < self.num_of_car:
             if self.car_list[id].orientation == "h":
                 if direction == 0:
@@ -123,9 +131,19 @@ class game:
                             print("the car is blocked by another car")
                     else:
                         print("cannot move car: it has reached the edge")
+            self.car_list[id].update_domain_and_range()
         else:
             print("invalid car id")
 
+        if id == 0:
+            #this is the winning car
+            print(self.car_list[0].row)
+            print(self.car_list[0].col)
+            if self.car_list[id].row == 2 and self.car_list[id].col   == 5:
+                self.car_list[id].ready_to_win = True
+                print(self.car_list[id].ready_to_win)
+            else:
+                self.car_list[id].ready_to_win = False
 
     def get_car_list(self):
         return self.car_list
@@ -138,27 +156,28 @@ class game:
             print("\n")
 
 def draw_grid(surface, size):
-    black = (0,0,0)
+    black = (150,150,150)
     for i in range(6):
         surface.fill(black, (0, i * size/6, size, 1))
         surface.fill(black, (i * size/6, 0,  1, size))
 
 def draw_car(surface, car, color):
     white = (255, 255, 255)
+    black = (255,255,255)
     if car.orientation == "h":
             surface.fill(color, ((car.col) * 80, (car.row)* 80, 80*car.size, 80))
             #border
-            surface.fill(white, ((car.col) * 80, (car.row) * 80, 80*car.size, 1)) #top hor
-            surface.fill(white, ((car.col) * 80, (car.row) *80 + 80, 80*car.size, 1)) #button hor
-            surface.fill(white, ((car.col) * 80, (car.row) * 80, 1, 80))#left ver
-            surface.fill(white, ((car.col) * 80 + 80 * car.size, (car.row) * 80, 1, 80))#right ver
+            surface.fill(black, ((car.col) * 80, (car.row) * 80, 80*car.size, 1)) #top hor
+            surface.fill(black, ((car.col) * 80, (car.row) *80 + 80, 80*car.size, 1)) #button hor
+            surface.fill(black, ((car.col) * 80, (car.row) * 80, 1, 80))#left ver
+            surface.fill(black, ((car.col) * 80 + 80 * car.size, (car.row) * 80, 1, 80))#right ver
     elif car.orientation == "v":
             surface.fill(color, ((car.col) * 80, (car.row) * 80, 80, 80*car.size))
             # border
-            surface.fill(white, ((car.col) * 80, (car.row) * 80, 80,1))  # top hor
-            surface.fill(white, ((car.col) * 80, (car.row) * 80 + 80 * car.size, 80, 1))  # button hor
-            surface.fill(white, ((car.col) * 80, (car.row) * 80, 1, 80 * car.size))  # left ver
-            surface.fill(white, ((car.col) * 80 + 80, (car.row) * 80, 1, 80* car.size))  # right ver
+            surface.fill(black, ((car.col) * 80, (car.row) * 80, 80,1))  # top hor
+            surface.fill(black, ((car.col) * 80, (car.row) * 80 + 80 * car.size, 80, 1))  # button hor
+            surface.fill(black, ((car.col) * 80, (car.row) * 80, 1, 80 * car.size))  # left ver
+            surface.fill(black, ((car.col) * 80 + 80, (car.row) * 80, 1, 80* car.size))  # right ver
 
 def load_carlist_from_file(filename):
     car_list = []
@@ -170,10 +189,6 @@ def load_carlist_from_file(filename):
         row = int(row)
         col = int(col)
         size = int(size)
-        print(orientation)
-        print(row)
-        print(col)
-        print(size)
         car_list.append(car(orientation, size, row, col, i))
         i += 1
     f.close()
@@ -185,68 +200,72 @@ def main():
     surface_sz = 480   # Desired physical surface size, in pixels.
     car_list = load_carlist_from_file("C:\\Users\\whkel\\Desktop\\car.txt")
     game1 = game(car_list[0])
-    game1.print_grid()
+
+    my_font = pygame.font.SysFont("Courier", 50)
 
     for i in range(1,len(car_list)):
         game1.add_car(car_list[i])
-    print("\n")
-    game1.print_grid()
-    print("\n")
+
 
 
     # Create surface of (width, height), and its window.
     main_surface = pygame.display.set_mode((surface_sz, surface_sz))
-
-    # Set up some data to describe a small rectangle and its color
-    small_rect = (300, 200, 150, 90)
-    horizontal_line = (0, 0, 480, 1)
-    horizontal_line2 = (0,80, 480, 1)
     black = (0, 0, 0)        # A color is a mix of (Red, Green, Blue)
-    white = (255, 255, 255)
-    red = (255, 0, 0)
-    green = (0, 100, 0)
-    blue = (0,0, 255)
-    ess = (0, 170, 171)
-    gold = (225, 225, 0)
-
-
-    color_list = []
-    color_list.append(white)
-    color_list.append(red)
-    color_list.append(green)
-    color_list.append(blue)
-    color_list.append(ess)
-    color_list.append(black)
-    color_list.append(white)
-    color_list.append(red)
-    color_list.append(green)
-    color_list.append(blue)
-    color_list.append(ess)
+    green = (0, 0, 120)
 
     while True:
         ev = pygame.event.poll()    # Look for any event
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+            clicked_pos = ev.dict["pos"]
+            for i in range(len(car_list)):
+                if car_list[i].contain_point(clicked_pos):
+                    #if mouse click is right on a car
+                    print(i)
+                    current_car = i
+        if ev.type == pygame.MOUSEBUTTONUP:
+            released_pos = ev.dict["pos"]
+            if car_list[current_car].orientation == "v":
+                if released_pos[1] > clicked_pos[1]:
+                    #move down
+                    game1.move_car(current_car, 1)
+                elif released_pos[1] < clicked_pos[1]:
+                    #move up
+                    game1.move_car(current_car, 0)
+            elif car_list[current_car].orientation == "h":
+                if released_pos[0] > clicked_pos[0]:
+                    #move right
+                    game1.move_car(current_car, 1)
+                elif released_pos[0] < clicked_pos[0]:
+                    #move left
+                    game1.move_car(current_car, 0)
+
+
+            #game1.print_grid()
+            print("\n")
         if ev.type == pygame.QUIT:  # Window close button clicked?
             break                   #   ... leave game loop
 
         # Update your game objects and data structures here...
 
-        game1.print_grid()
+        #game1.print_grid()
         # We draw everything from scratch on each frame.
         # So first fill everything with the background color
-        main_surface.fill((100, 100, 100))
-        draw_grid(main_surface, 480)
-        if game1.win != True:
-            draw_car(main_surface, game1.car_list[0], gold)
-        for j in range(len(car_list)):
-            draw_car(main_surface, game1.car_list[j], black)
-            if j == 0:
-                draw_car(main_surface, game1.car_list[0], gold)
+        main_surface.fill((50, 50, 50))
+        if not game1.win:
+            draw_grid(main_surface, 480)
+            if game1.win != True:
+                draw_car(main_surface, game1.car_list[0], green)
+            for j in range(len(car_list)):
+                draw_car(main_surface, game1.car_list[j], black)
+                if j == 0:
+                    draw_car(main_surface, game1.car_list[0], green)
 
-        # display
+        else:
+            label = my_font.render("YOU WIN!!", 1, (255, 255, 0))
+            main_surface.blit(label, (110, 200))
+
+        #display
         pygame.display.flip()
-        which_car = int(input("which car do you want to move?"))
-        direction = int(input("what direction would you like to move?"))
-        game1.move_car(which_car, direction)
 
     pygame.quit()     # Once we leave the loop, close the window.
 
--- list_t.py
+++
@@ -1,7 +0,0 @@
-def listfun(list):
-    return list
-
-
-x = [1, 1, 1]
-y = listfun(x)
-print(y)
